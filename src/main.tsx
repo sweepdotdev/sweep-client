c5c60e44a62514807249f75327417279b90562a8
metal-face <hughesbryan3000@gmail.com>
2024-10-02 18:09:42 -0400

    main refactored

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 import "./index.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <ThemeProvider defaultTheme={"dark"} storageKey={"sweep-ui-theme"}>
+            <RouterProvider router={router} />
+        </ThemeProvider>
     </StrictMode>,
 );
