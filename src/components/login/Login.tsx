87f60270093ad5e0878d69e08e2db8e62b6f3544
metal-face <hughesbryan3000@gmail.com>
2024-10-02 18:09:37 -0400

    login component started

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function Login(): ReactElement {
+    return (
+        <div className={"h-full w-full"}>
+            <div>ligma</div>
+        </div>
+    );
+}
