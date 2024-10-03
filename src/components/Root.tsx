eae7e8759d00cbdc47a7fcb232b52cce8ce0aca5
metal-face <hughesbryan3000@gmail.com>
2024-10-03 17:39:09 -0400

    login outlet added

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,14 @@
 import { ReactElement } from "react";
 import AppBar from "./root/app-bar.tsx";
+import { Outlet } from "react-router-dom";
 
 export default function Root(): ReactElement {
     return (
         <div className={"h-screen w-screen"}>
             <AppBar />
+            <div id={"login"} className={"h-full w-full"}>
+                <Outlet />
+            </div>
         </div>
     );
 }
