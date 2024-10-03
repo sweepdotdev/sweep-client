d5fa307db70119de7eabce0f76a7efb9018887f2
metal-face <hughesbryan3000@gmail.com>
2024-10-03 18:29:22 -0400

    Tanstack Query added to project

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <ThemeProvider defaultTheme={"light"} storageKey={"sweep-ui-theme"}>
-            <RouterProvider router={router} />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider defaultTheme={"light"} storageKey={"sweep-ui-theme"}>
+                <RouterProvider router={router} />
+            </ThemeProvider>
+            <ReactQueryDevtools />
+        </QueryClientProvider>
     </StrictMode>,
 );
