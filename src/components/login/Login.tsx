fad958327d5c65249697a3411968a4939edfe13d
metal-face <hughesbryan3000@gmail.com>
2024-10-03 17:39:44 -0400

    login page written

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,9 +1,84 @@
 import { ReactElement } from "react";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../ui/card.tsx";
+import { Link } from "react-router-dom";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "../../schemas/login.ts";
+import { Button } from "../ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
 
 export default function Login(): ReactElement {
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    async function onSubmit(values: z.infer<typeof loginSchema>) {
+        console.log(values);
+    }
     return (
-        <div className={"h-full w-full"}>
-            <div>ligma</div>
+        <div className={"h-full w-full flex justify-center items-center"}>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Login</CardTitle>
+                    <CardDescription>
+                        Enter your email and password to login into your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name={"email"}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name={"password"}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type={"submit"}>Submit</Button>
+                        </form>
+                    </Form>
+                </CardContent>
+                <CardFooter>
+                    <p className={"text-xs"}>
+                        Don't have an account?{" "}
+                        <Link to={"/register"} className={"hover:underline text-blue-500"}>
+                            Register
+                        </Link>
+                    </p>
+                </CardFooter>
+            </Card>
         </div>
     );
 }
