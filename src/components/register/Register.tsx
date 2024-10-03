b0f3591e00437f0a4017dbd7ce279a90fbfea230
metal-face <hughesbryan3000@gmail.com>
2024-10-03 17:39:03 -0400

    register page started

---
+++ src/components/register/Register.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+import { Card } from "../ui/card.tsx";
+import { registerSchema } from "../../schemas/register.ts";
+
+export default function Register(): ReactElement {
+    return (
+        <div>
+            <Card></Card>
+        </div>
+    );
+}
