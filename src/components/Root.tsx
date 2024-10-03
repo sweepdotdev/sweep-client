59a7ff247f6970fb54426fc7e4eb765ccdd7337a
metal-face <hughesbryan3000@gmail.com>
2024-10-03 17:53:10 -0400

    outlet for register added

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -9,6 +9,9 @@ export default function Root(): ReactElement {
             <div id={"login"} className={"h-full w-full"}>
                 <Outlet />
             </div>
+            <div id={"register"} className={"h-full w-full"}>
+                <Outlet />
+            </div>
         </div>
     );
 }
