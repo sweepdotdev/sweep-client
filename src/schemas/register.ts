5c6ce8c9eaccbbc24cafaa085b6a59adacae6147
metal-face <hughesbryan3000@gmail.com>
2024-10-04 12:45:23 -0400

    password strength test added to register from login

--- src/schemas/register.ts
+++ src/schemas/register.ts
@@ -6,14 +6,48 @@ export const registerSchema = z
         lastName: z.string().min(2).max(28),
         company: z.string().min(2).max(28).optional(),
         email: z.string().email(),
-        password: z
-            .string()
-            .min(12, { message: "Password must be at least 12 characters." })
-            .max(32, { message: "Password cannot be longer than 32 characters." }),
-        confirm: z
-            .string()
-            .min(12, { message: "Password must be at least 12 characters." })
-            .max(32, { message: "Password cannot be longer than 32 characters." }),
+        password: z.string().min(12, { message: "Password must be at least 12 characters." }),
+        confirm: z.string().min(12, { message: "Password must be at least 12 characters." }),
+    })
+    .superRefine(({ password }, checkPassComplexity) => {
+        const containsUppercase = (ch: string) => /[A-Z]/.test(ch);
+        const containsLowercase = (ch: string) => /[a-z]/.test(ch);
+        const containsSpecialChar = (ch: string) =>
+            /[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/.test(ch);
+
+        const containsNumber = (ch: string) => /[0-9]/.test(ch);
+
+        if (!containsSpecialChar(password)) {
+            checkPassComplexity.addIssue({
+                code: "custom",
+                message: "Password must contain a special character",
+                path: ["password"],
+            });
+        }
+
+        if (!containsUppercase(password)) {
+            checkPassComplexity.addIssue({
+                code: "custom",
+                message: "Password must contain an uppercase character!",
+                path: ["password"],
+            });
+        }
+
+        if (!containsLowercase(password)) {
+            checkPassComplexity.addIssue({
+                code: "custom",
+                message: "Password must contain an lowercase character!",
+                path: ["password"],
+            });
+        }
+
+        if (!containsNumber(password)) {
+            checkPassComplexity.addIssue({
+                code: "custom",
+                message: "Password must contain a number!",
+                path: ["password"],
+            });
+        }
     })
     .superRefine(({ password, confirm }, ctx) => {
         if (password !== confirm) {
