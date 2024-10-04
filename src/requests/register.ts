a735c7c2e355e8979278fca478663a26d103a036
metal-face <hughesbryan3000@gmail.com>
2024-10-04 12:45:08 -0400

    register request defined

---
+++ src/requests/register.ts
@@ -0,0 +1,25 @@
+import { AxiosResponse } from "axios";
+import { Axios } from "../lib/axios";
+
+export default async function register(
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    company: string,
+): Promise<AxiosResponse> {
+    return Axios({
+        method: "POST",
+        url: "http://localhost:8000/users",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+            first_name: firstName,
+            last_name: lastName,
+            company: company,
+            email: email,
+            password: password,
+        }),
+    });
+}
