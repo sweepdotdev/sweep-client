5a20ae73d21fbfdaba7245558f7e63eb163b4aac
metal-face <hughesbryan3000@gmail.com>
2024-10-05 19:54:50 -0400

    shadcn toast provider added to root

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { router } from "./router";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./components/ui/toaster.tsx";
 import "./index.css";
 
 const queryClient = new QueryClient();
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
         <QueryClientProvider client={queryClient}>
             <ThemeProvider defaultTheme={"light"} storageKey={"sweep-ui-theme"}>
                 <RouterProvider router={router} />
+                <Toaster />
             </ThemeProvider>
             <ReactQueryDevtools />
         </QueryClientProvider>
