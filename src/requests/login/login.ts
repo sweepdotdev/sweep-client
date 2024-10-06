21a82598b82bfc0637b93deff0be2a93376e5b4c
metal-face <hughesbryan3000@gmail.com>
2024-10-06 15:44:21 -0400

    login payload typed

--- src/requests/login/login.ts
+++ src/requests/login/login.ts
@@ -1,7 +1,8 @@
 import { AxiosResponse } from "axios";
 import { Axios } from "../../lib/axios.ts";
+import { LoginData } from "./use-login.tsx";
 
-export default async function login(email: string, password: string): Promise<AxiosResponse> {
+export default async function login({ email, password }: LoginData): Promise<AxiosResponse> {
     return Axios({
         method: "POST",
         url: "http://localhost:8000/v1/users/self/authenticate",
