0a492cea0bf3ed001194a46245ce95ce30c6c4e8
metal-face <hughesbryan3000@gmail.com>
2024-10-06 15:43:50 -0400

    useLogin hook written for login mutation

---
+++ src/requests/login/use-login.tsx
@@ -0,0 +1,33 @@
+import { useMutation } from "@tanstack/react-query";
+import { useToast } from "../../hooks/use-toast.ts";
+import login from "./login.ts";
+import { AxiosResponse } from "axios";
+
+export interface LoginData {
+    email: string;
+    password: string;
+}
+
+export default function useLogin() {
+    const { toast } = useToast();
+    return useMutation({
+        mutationKey: ["loginUser"],
+        mutationFn: async ({ email, password }: LoginData): Promise<AxiosResponse> => {
+            return await login({ email: email, password: password });
+        },
+        onSuccess: async () => {
+            toast({
+                title: "Success! 🎉",
+                description: "You have successfully logged in to your account!",
+                className: "bg-green-500",
+            });
+        },
+        onError: async (): Promise<void> => {
+            toast({
+                title: "Oops! 😬",
+                description: "Something went wrong!",
+                variant: "destructive",
+            });
+        },
+    });
+}
