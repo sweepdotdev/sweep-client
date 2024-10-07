fac4643eb2bd6c2c8c22328fef1a8f83ca21f6c4
metal-face <hughesbryan3000@gmail.com>
2024-10-06 21:26:33 -0400

    StoreProvider added to Proivder tree

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "./components/theme-provider.tsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster.tsx";
+import { StoreProvider } from "./lib/zustand.tsx";
 import "./index.css";
 
 const queryClient = new QueryClient();
@@ -13,8 +14,10 @@ createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <ThemeProvider defaultTheme={"light"} storageKey={"sweep-ui-theme"}>
-                <RouterProvider router={router} />
-                <Toaster />
+                <StoreProvider>
+                    <RouterProvider router={router} />
+                    <Toaster />
+                </StoreProvider>
             </ThemeProvider>
             <ReactQueryDevtools />
         </QueryClientProvider>
