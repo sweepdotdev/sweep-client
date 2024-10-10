3a9d04259eb27656a9dd1e8633a8e2d84a51cb21
Josh <joshua.smith@super.com>
2024-10-10 00:02:03 -0700

    public key

--- src/lib/security.ts
+++ src/lib/security.ts
@@ -2,7 +2,7 @@ import { JWTVerifyResult, KeyLike } from "jose";
 import * as jose from "jose";
 
 export default async function verifyJWT(idCookie: string): Promise<JWTVerifyResult> {
-    const rawPublicKey: string = (import.meta.env.VITE_JWT_SECRET_KEY as string) || "";
+    const rawPublicKey: string = (import.meta.env.VITE_JWT_PUBLIC_KEY as string) || "";
     const publicKey: KeyLike = await jose.importSPKI(rawPublicKey, "RS256");
 
     try {
