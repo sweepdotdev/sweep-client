2af153df8f53025bd88ebcf87819668ea3036c28
metal-face <hughesbryan3000@gmail.com>
2024-10-14 02:43:01 -0400

    user-dropdown.tsx added to app-bar.tsx

--- src/components/root/app-bar.tsx
+++ src/components/root/app-bar.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { House, LogIn } from "lucide-react";
 import { ModeToggle } from "./mode-toggle.tsx";
 import { useStoreInContext } from "../../lib/zustand.tsx";
+import UserDropdown from "./user-dropdown.tsx";
 
 export default function AppBar(): ReactElement {
     const loggedIn: boolean = useStoreInContext((state) => state.loggedIn);
@@ -22,7 +23,7 @@ export default function AppBar(): ReactElement {
                 "absolute w-full h-[100px] dark:bg-neutral-600 bg-neutral-300 flex justify-end items-center"
             }
         >
-            <div className={"p-4 space-x-4 flex"}>
+            <div className={"p-4 space-x-4 flex items-center"}>
                 <Link
                     to={"/"}
                     className={
@@ -31,6 +32,8 @@ export default function AppBar(): ReactElement {
                 >
                     Home <House className={"ml-2 h-4 w-4"} />
                 </Link>
+
+                {authenticated ? <UserDropdown /> : <></>}
 
                 {!authenticated ? (
                     <Link
