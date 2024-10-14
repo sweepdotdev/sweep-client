87148b31ec6cc2de407dc827972314a1042cd269
metal-face <hughesbryan3000@gmail.com>
2024-10-14 05:23:45 -0400

    copy invite code to clipboard added

--- src/components/user-information/user-info.tsx
+++ src/components/user-information/user-info.tsx
@@ -5,28 +5,55 @@ import { useStoreInContext } from "../../lib/zustand.tsx";
 import { useQuery } from "@tanstack/react-query";
 import { Label } from "../ui/label.tsx";
 import { getInviteCode } from "../../requests/user/get-invite-code.ts";
+import { Button } from "../ui/button.tsx";
+import { Copy } from "lucide-react";
+import { useToast } from "../../hooks/use-toast.ts";
 
 export default function UserInfo(): ReactElement {
     const { organization, email, lastName, firstName } = useStoreInContext((state) =>
         state.getState(),
     );
-
     const [loaded, setLoaded] = useState<boolean>(false);
-    const inviteCode = useQuery({
+    const inviteCodeQuery = useQuery({
         queryKey: ["inviteCode", organization],
         queryFn: async () => {
             return await getInviteCode({ organizationId: organization });
         },
     });
+    const [inviteCode, setInviteCode] = useState<string>("");
+    const { toast } = useToast();
 
     useEffect(() => {
         if (!loaded) {
             setLoaded(true);
         }
+    }, []);
+
+    useEffect(() => {
         if (loaded) {
-            console.log(inviteCode.data?.data);
+            setInviteCode(inviteCodeQuery.data?.data.invite_code);
         }
     }, [loaded]);
+
+    async function copyToClipboard() {
+        try {
+            await navigator.clipboard.writeText(
+                `http://localhost:5173/register/user/${inviteCode}`,
+            );
+            toast({
+                title: "Copied! 💾",
+                description: "You've copied the invite code to your clipboard.",
+                className: "bg-green-500",
+            });
+        } catch (error) {
+            console.error(error);
+            toast({
+                title: "Copy Failed!",
+                description: "Something went wrong!",
+                variant: "destructive",
+            });
+        }
+    }
 
     return (
         <div className={"h-full w-full flex items-center justify-center"}>
@@ -52,11 +79,17 @@ export default function UserInfo(): ReactElement {
                     </div>
                     <div>
                         <Label htmlFor={"inviteCode"}>Invite Code</Label>
-                        <Input
-                            id={"inviteCode"}
-                            value={`http://localhost:5173/register/user/${inviteCode.data?.data.invite_code}`}
-                            readOnly
-                        />
+                        <div className={"flex items-center space-x-2"}>
+                            <Input
+                                className={"text-xs"}
+                                id={"inviteCode"}
+                                value={`http://localhost:5173/register/user/${inviteCode}`}
+                                readOnly
+                            />
+                            <Button size={"sm"} onClick={async () => await copyToClipboard()}>
+                                <Copy className={"h-4 w-4"} />
+                            </Button>
+                        </div>
                     </div>
                 </CardContent>
             </Card>
