85861481d5009442a37b17a26ef26b2c20f929b5
metal-face <hughesbryan3000@gmail.com>
2024-10-14 23:35:04 -0400

    account id param removed

--- src/components/root/user-dropdown.tsx
+++ src/components/root/user-dropdown.tsx
@@ -10,30 +10,17 @@ import {
 } from "../ui/dropdown-menu.tsx";
 import { Button } from "../ui/button.tsx";
 import { PowerIcon, CircleUser } from "lucide-react";
-import { useToast } from "../../hooks/use-toast.ts";
 import { Link, NavigateFunction, useNavigate } from "react-router-dom";
 import useLogout from "../../requests/logout/use-logout.tsx";
 
 export default function UserDropdown(): ReactElement {
     const getEmail: string = useStoreInContext((state) => state.email);
-    const accountId: string = useStoreInContext((state) => state.sub);
     const clearState: () => void = useStoreInContext((state) => state.clearState);
     const redirect: NavigateFunction = useNavigate();
-    const { toast } = useToast();
     const logoutMutation = useLogout();
 
     async function logoutUser() {
-        if (!accountId) {
-            toast({
-                title: "Oops! 😬",
-                description: "No account ID present!",
-                variant: "destructive",
-            });
-
-            return;
-        }
-
-        await logoutMutation.mutateAsync(accountId);
+        await logoutMutation.mutateAsync();
 
         clearState();
 
