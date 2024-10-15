9d522ec7798df581e13023b25495883d5e737ae6
metal-face <hughesbryan3000@gmail.com>
2024-10-14 23:33:28 -0400

    organization ID added back to request

--- src/requests/user/get-invite-code.ts
+++ src/requests/user/get-invite-code.ts
@@ -1,10 +1,17 @@
 import { Axios } from "../../lib/axios.ts";
 import { AxiosResponse } from "axios";
 
-export async function getInviteCode(): Promise<AxiosResponse> {
+export interface InviteCodeRequestData {
+    organizationId: string;
+}
+
+export async function getInviteCode({
+    organizationId,
+}: InviteCodeRequestData): Promise<AxiosResponse> {
     return Axios({
         method: "GET",
-        url: `http://localhost:8000/v1/invite-codes`,
+        url: `http://localhost:8000/v1/organizations/${organizationId}/invite_codes`,
+        withCredentials: true,
         headers: {
             "Content-Type": "application/json",
         },
