1c7446b044dac4c5f6c31ee7dd4af9d931a19cc6
metal-face <hughesbryan3000@gmail.com>
2024-10-19 16:15:06 -0400

    create change request component started

---
+++ src/components/change-requests/CreateChangeRequests.tsx
@@ -0,0 +1,42 @@
+import { ReactElement, useEffect } from "react";
+import { useStoreInContext } from "../../lib/zustand.tsx";
+import { useNavigate } from "react-router-dom";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../ui/card.tsx";
+import { Button } from "../ui/button.tsx";
+
+export default function CreateChangeRequests(): ReactElement {
+    const loggedIn: boolean = useStoreInContext((state) => state.loggedIn);
+    const redirect = useNavigate();
+
+    useEffect(() => {
+        if (!loggedIn) {
+            redirect("/login");
+        }
+    }, [loggedIn]);
+
+    return (
+        <div className={"h-full w-full flex justify-center items-center"}>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Create a Change Request</CardTitle>
+                    <CardDescription>
+                        Enter the information below to create a change request
+                    </CardDescription>
+                </CardHeader>
+                <CardContent></CardContent>
+                <CardFooter>
+                    <div className={"w-full flex justify-end"}>
+                        <Button>Submit</Button>
+                    </div>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+}
