094de729441e88f57a58d9cc5b87ea3ead958220
metal-face <hughesbryan3000@gmail.com>
2024-10-21 17:25:38 -0400

    sidebar trigger styled

--- src/components/root/app-bar.tsx
+++ src/components/root/app-bar.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, useEffect, useState } from "react";
 import { Link, Location, useLocation } from "react-router-dom";
-import { House, LogIn } from "lucide-react";
+import { Compass, House, LogIn } from "lucide-react";
 import { ModeToggle } from "@/components/root/mode-toggle.tsx";
 import { useStoreInContext } from "@/lib/zustand.tsx";
 import { Button } from "@/components/ui/button.tsx";
@@ -25,7 +25,13 @@ export default function AppBar(): ReactElement {
                 "absolute w-full h-[100px] dark:bg-neutral-600 bg-neutral-300 flex justify-end items-center"
             }
         >
-            <div>{authenticated ? <SidebarTrigger /> : <></>}</div>
+            <SidebarTrigger
+                className={
+                    "bg-neutral-200 dark:bg-neutral-800 hover:bg-neutral-300 hover:dark:bg-neutral-800 focus-visible:ring-0"
+                }
+            >
+                <Compass className={"h-4 w-4 text-black dark:text-white"} />
+            </SidebarTrigger>
             <div className={"p-4 space-x-4 flex items-center"}>
                 {authenticated && pathname !== "/" ? (
                     <Link to={"/"} className={"text-black dark:text-white flex items-center"}>
