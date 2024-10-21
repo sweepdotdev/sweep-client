57224763270d2f2d8f6265e2ded489e676813646
metal-face <hughesbryan3000@gmail.com>
2024-10-21 17:04:29 -0400

    import aliasing solved

--- src/requests/integrations/oauth2/github.ts
+++ src/requests/integrations/oauth2/github.ts
@@ -1,16 +1,17 @@
 import { AxiosResponse } from "axios";
-import { Axios } from "../../../lib/axios.ts";
+import { Axios } from "@/lib/axios.ts";
 
 export type ProviderName = "github";
 
 export interface GithubAuthorizationData {
-    providerName: ProviderName
+    providerName: ProviderName;
     authorizationCode: string;
 }
 
-export default async function saveUserGithubAuthorization(
-    { providerName, authorizationCode }: GithubAuthorizationData
-): Promise<AxiosResponse> {
+export default async function saveUserGithubAuthorization({
+    providerName,
+    authorizationCode,
+}: GithubAuthorizationData): Promise<AxiosResponse> {
     return Axios({
         method: "POST",
         url: `${import.meta.env.VITE_BACKEND_BASE_URL}/v1/users/self/sso/${providerName}`,
