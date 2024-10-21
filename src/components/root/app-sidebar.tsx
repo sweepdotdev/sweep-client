a379c142f4c0f710cbfea89cdacda7ab3614b7e9
metal-face <hughesbryan3000@gmail.com>
2024-10-21 18:10:05 -0400

    sidebar now closable by clicking off of it

--- src/components/root/app-sidebar.tsx
+++ src/components/root/app-sidebar.tsx
@@ -1,14 +1,27 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
     SidebarHeader,
+    useSidebar,
 } from "@/components/ui/sidebar";
 import UserDropdown from "@/components/root/user-dropdown.tsx";
 
-export function AppSidebar(): ReactElement {
+interface Props {
+    triggerSetOpen: boolean;
+}
+
+export function AppSidebar({ triggerSetOpen }: Props): ReactElement {
+    const { setOpen } = useSidebar();
+
+    useEffect(() => {
+        if (triggerSetOpen) {
+            setOpen(false);
+        }
+    }, [triggerSetOpen]);
+
     return (
         <Sidebar variant={"sidebar"}>
             <SidebarHeader />
