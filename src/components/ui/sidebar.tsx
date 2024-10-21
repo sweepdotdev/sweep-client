a13e50a74a5c5ed224e1a4c6581ae9919a6cff9a
metal-face <hughesbryan3000@gmail.com>
2024-10-21 17:25:30 -0400

    sidebar trigger adjusted to have custom logic

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
-import { PanelLeft } from "lucide-react";
 
 import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
@@ -249,24 +248,21 @@ Sidebar.displayName = "Sidebar";
 const SidebarTrigger = React.forwardRef<
     React.ElementRef<typeof Button>,
     React.ComponentProps<typeof Button>
->(({ className, onClick, ...props }, ref) => {
+>(({ children, className, onClick, ...props }, ref) => {
     const { toggleSidebar } = useSidebar();
 
     return (
         <Button
             ref={ref}
             data-sidebar="trigger"
-            variant="ghost"
-            size="icon"
-            className={cn("h-7 w-7", className)}
+            className={className}
             onClick={(event) => {
                 onClick?.(event);
                 toggleSidebar();
             }}
             {...props}
         >
-            <PanelLeft />
-            <span className="sr-only">Toggle Sidebar</span>
+            {children}
         </Button>
     );
 });
