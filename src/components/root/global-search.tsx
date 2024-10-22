d1ee5d632c913018152bd9823436ca91c13ce1ee
metal-face <hughesbryan3000@gmail.com>
2024-10-22 15:29:00 -0400

    Command component removed, not correct

--- src/components/root/global-search.tsx
+++ src/components/root/global-search.tsx
@@ -55,10 +55,10 @@ export default function GlobalSearch(): ReactElement {
             <Button
                 onClick={() => setOpen(!open)}
                 className={
-                    "dark:text-white text-black bg-neutral-200 dark:bg-neutral-800 hover:bg-neutral-300 hover:dark:bg-neutral-800 focus-visible:ring-0"
+                    "mr-2 dark:text-white text-black bg-neutral-200 dark:bg-neutral-800 hover:bg-neutral-300 hover:dark:bg-neutral-800 focus-visible:ring-0"
                 }
             >
-                Press{" "}
+                Search{" "}
                 <kbd className="ml-2 pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
                     <span className="text-xs">⌘</span>K
                 </kbd>
