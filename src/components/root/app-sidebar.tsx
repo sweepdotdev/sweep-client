7d1f07f6fb617c676934c4a7cb49a07282db96de
metal-face <hughesbryan3000@gmail.com>
2024-10-21 22:25:05 -0400

    sidebar built out

--- src/components/root/app-sidebar.tsx
+++ src/components/root/app-sidebar.tsx
@@ -1,36 +1,90 @@
-import { ReactElement, useEffect } from "react";
+import { ForwardRefExoticComponent, ReactElement, RefAttributes, useEffect } from "react";
 import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
+    SidebarGroupContent,
     SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
     useSidebar,
 } from "@/components/ui/sidebar";
 import UserDropdown from "@/components/root/user-dropdown.tsx";
+import { Home, GitPullRequestCreate, LucideProps, GitPullRequest } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
-interface Props {
-    triggerSetOpen: boolean;
+interface NavItem {
+    title: string;
+    url: string;
+    icon: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>;
 }
 
-export function AppSidebar({ triggerSetOpen }: Props): ReactElement {
+export function AppSidebar(): ReactElement {
     const { setOpen } = useSidebar();
 
     useEffect(() => {
-        if (triggerSetOpen) {
-            setOpen(false);
-        }
-    }, [triggerSetOpen]);
+        window.addEventListener("keydown", (e) => {
+            if (e.key === "Escape") {
+                setOpen(false);
+            }
+        });
+    }, []);
+
+    const items: NavItem[] = [
+        {
+            title: "Home",
+            url: "/",
+            icon: Home,
+        },
+        {
+            title: "Change Requests",
+            url: "/change-requests",
+            icon: GitPullRequest,
+        },
+        {
+            title: "Create Change Request",
+            url: "/change-requests/create",
+            icon: GitPullRequestCreate,
+        },
+    ];
 
     return (
         <Sidebar variant={"sidebar"}>
             <SidebarHeader />
             <SidebarContent>
-                <SidebarGroup />
-                <SidebarGroup />
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item: NavItem) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
             </SidebarContent>
             <SidebarFooter>
-                <UserDropdown />
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <UserDropdown />
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                        <Button
+                            className={"w-full bg-red-500 hover:bg-red-500"}
+                            onClick={() => setOpen(false)}
+                        >
+                            Close Menu
+                        </Button>
+                    </SidebarMenuItem>
+                </SidebarMenu>
             </SidebarFooter>
         </Sidebar>
     );
