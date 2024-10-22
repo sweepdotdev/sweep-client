f2b5491ee825b22bf0e8a2fc32ba08029625c43d
metal-face <hughesbryan3000@gmail.com>
2024-10-22 16:31:24 -0400

    create change request form built out

--- src/components/change-requests/CreateChangeRequests.tsx
+++ src/components/change-requests/CreateChangeRequests.tsx
@@ -1,6 +1,13 @@
-import { ReactElement, useEffect } from "react";
-import { useStoreInContext } from "@/lib/zustand.tsx";
-import { useNavigate } from "react-router-dom";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+
 import {
     Card,
     CardContent,
@@ -9,7 +16,16 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card.tsx";
+
+import { z } from "zod";
+import { ReactElement, useEffect } from "react";
+import { useStoreInContext } from "@/lib/zustand.tsx";
+import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button.tsx";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { changeRequestSchema } from "@/schemas/change-requests";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function CreateChangeRequests(): ReactElement {
     const loggedIn: boolean = useStoreInContext((state) => state.loggedIn);
@@ -21,6 +37,24 @@ export default function CreateChangeRequests(): ReactElement {
         }
     }, [loggedIn]);
 
+    const form = useForm<z.infer<typeof changeRequestSchema>>({
+        resolver: zodResolver(changeRequestSchema),
+        defaultValues: {
+            command: "",
+            packageManager: "",
+            packageManagerVersion: "",
+            customCommitMessage: "",
+            customBranchName: "",
+            customPullRequestTitle: "",
+            eligibleGitNamespaces: [""],
+            dryRun: true,
+        },
+    });
+
+    async function onSubmit(values: z.infer<typeof changeRequestSchema>): Promise<void> {
+        console.log(values);
+    }
+
     return (
         <div className={"h-full w-full flex justify-center items-center"}>
             <Card>
@@ -30,7 +64,94 @@ export default function CreateChangeRequests(): ReactElement {
                         Enter the information below to create a change request
                     </CardDescription>
                 </CardHeader>
-                <CardContent></CardContent>
+                <CardContent>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name={"command"}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Command</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            Enter the command you would like to apply across
+                                            repositories.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name={"packageManager"}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Package Manager</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name={"packageManagerVersion"}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Package Manager Version</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name={"customCommitMessage"}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Commit Message</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name={"customBranchName"}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Branch Name</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name={"customPullRequestTitle"}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Pull Request Title</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </form>
+                    </Form>
+                </CardContent>
                 <CardFooter>
                     <div className={"w-full flex justify-end"}>
                         <Button>Submit</Button>
