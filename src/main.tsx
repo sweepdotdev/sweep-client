ef1b2f8930d3f656a4538a8b6717551787ebd390
metal-face <hughesbryan3000@gmail.com>
2024-10-24 18:49:38 -0400

    small cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster.tsx";
 import { StoreProvider } from "./lib/zustand.tsx";
 import { TooltipProvider } from "./components/ui/tooltip.tsx";
+import { router } from "./router";
 import "./global.css";
 
 const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
