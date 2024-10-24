4f113052b508561f0594a8ab124df5292d217800
metal-face <hughesbryan3000@gmail.com>
2024-10-24 18:49:47 -0400

    stripe intent requested

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,15 +1,36 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { NavigateFunction, Outlet, useNavigate } from "react-router-dom";
 import { useStoreInContext } from "@/lib/zustand.tsx";
 import { isAfter } from "date-fns";
 import { AppSidebar } from "@/components/root/app-sidebar.tsx";
 import { SidebarProvider } from "@/components/ui/sidebar.tsx";
+import { AxiosResponse } from "axios";
+import { Loader } from "lucide-react";
 import AppBar from "@/components/root/app-bar.tsx";
+import useIntentMutation from "@/requests/billing/use-intent-mutation";
 
 export default function Root(): ReactElement {
     const sessionExpiry: Date | null = useStoreInContext((state) => state.expires);
     const redirect: NavigateFunction = useNavigate();
     const now = new Date();
+    const [clientSecret, setClientSecret] = useState("");
+    const intentMutation = useIntentMutation();
+
+    useEffect(() => {
+        intentMutation.mutateAsync().then((res: AxiosResponse) => {
+            setClientSecret(res.data.client_secret);
+        });
+    }, []);
+
+    console.log(clientSecret);
+
+    if (intentMutation.isPending) {
+        return (
+            <div className={"h-screen w-screen flex items-center justify-center"}>
+                <Loader className={"h-10 w-10 animate-spin"} />
+            </div>
+        );
+    }
 
     if (sessionExpiry && isAfter(now, sessionExpiry)) {
         redirect("/login");
