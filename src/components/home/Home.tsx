6ea75d8d0cc8899ad49312928dcc41003d46417f
metal-face <hughesbryan3000@gmail.com>
2024-10-25 18:57:07 -0400

    query made to check for oauth linkage and set result in state

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,6 +3,9 @@ import { useStoreInContext } from "@/lib/zustand";
 import { ProviderName } from "@/requests/integrations/oauth2/github";
 import { GitConnectionPrompt } from "@/components/home/GitConnectionPrompt";
 import { NavigateFunction, useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { confirmOAuthLinkage } from "@/requests/integrations/oauth2/confirm-linkage";
+import { Loader } from "lucide-react";
 
 const ACCEPTED_GIT_PROVIDERS = new Set(["github"]);
 
@@ -17,6 +20,25 @@ export default function Home(): ReactElement {
             redirect("/login");
         }
     }, []);
+
+    const setOAuth2Linkage = useStoreInContext((state) => state.setHasOAuth2Credentials);
+
+    const OAuthQuery = useQuery({
+        queryKey: ["confirm-linkage"],
+        queryFn: async () => await confirmOAuthLinkage(),
+    });
+
+    if (OAuthQuery.isLoading) {
+        return (
+            <div className={"h-screen w-screen flex items-center justify-center"}>
+                <Loader className={"h-10 w-10 animate-spin"} />
+            </div>
+        );
+    }
+
+    if (OAuthQuery.data?.status === 201) {
+        setOAuth2Linkage();
+    }
 
     useEffect(() => {
         const exists: boolean = providers.some((p) => ACCEPTED_GIT_PROVIDERS.has(p));
