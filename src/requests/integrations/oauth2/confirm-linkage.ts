6652482d9729fc2176e6a467db50c1b43ef74d78
metal-face <hughesbryan3000@gmail.com>
2024-10-25 18:56:42 -0400

    Request written to query for oauth linkage

---
+++ src/requests/integrations/oauth2/confirm-linkage.ts
@@ -0,0 +1,13 @@
+import { Axios } from "@/lib/axios";
+import { AxiosResponse } from "axios";
+
+export async function confirmOAuthLinkage(): Promise<AxiosResponse> {
+    return Axios({
+        method: "GET",
+        url: `${import.meta.env.VITE_BACKEND_BASE_URL}/v1/users/self/sso/confirm-linkage`,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        withCredentials: true,
+    });
+}
