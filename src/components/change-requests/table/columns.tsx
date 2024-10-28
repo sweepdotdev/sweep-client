a2b065b40eb91904ac66a2cf62d289f0003a1bed
metal-face <hughesbryan3000@gmail.com>
2024-10-28 01:04:01 -0400

    basic formatting added to certain fields

--- src/components/change-requests/table/columns.tsx
+++ src/components/change-requests/table/columns.tsx
@@ -1,4 +1,8 @@
 import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox";
+import { ReactElement } from "react";
+import { format } from "date-fns";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
 export type ChangeRequest = {
     packageManager: string;
@@ -9,7 +13,22 @@ export type ChangeRequest = {
     customCommitMessage: string;
     customPullRequestTitle: string;
     dryRun: boolean;
+    status: "pending" | "in_progress" | "completed";
+    createdAt: string;
 };
+
+function determineStatusColor(status: string): string {
+    switch (status) {
+        case "pending":
+            return "text-orange-500";
+        case "in_progress":
+            return "text-yellow-500";
+        case "completed":
+            return "text-green-500";
+        default:
+            return "text-orange-500";
+    }
+}
 
 export const columns: ColumnDef<ChangeRequest>[] = [
     {
@@ -37,7 +56,49 @@ export const columns: ColumnDef<ChangeRequest>[] = [
         header: "Pull Request Title",
     },
     {
+        accessorKey: "status",
+        header: "Status",
+        cell: ({ row }): ReactElement => {
+            const status: string = row.getValue("status");
+            const modifiedStatus: string = status.charAt(0).toUpperCase() + status.slice(1);
+            return <div className={`${determineStatusColor(status)}`}>{modifiedStatus}</div>;
+        },
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Creation Date",
+        cell: ({ row }): ReactElement => {
+            const rawDate: string = row.getValue("createdAt");
+            if (!rawDate) {
+                return <></>;
+            }
+            const modifiedDate: string = format(new Date(rawDate), "PPPP");
+            const modifiedTime: string = format(new Date(rawDate), "pppp");
+            return (
+                <Tooltip>
+                    <TooltipTrigger>
+                        <div>{modifiedDate}</div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p className={"text-md"}>{modifiedTime}</p>
+                    </TooltipContent>
+                </Tooltip>
+            );
+        },
+    },
+    {
         accessorKey: "dryRun",
         header: "Dry Run",
+        cell: ({ row }) => {
+            return (
+                <div className={"flex justify-between items-center"}>
+                    <Checkbox
+                        aria-readonly
+                        checked={row.getValue("dryRun")}
+                        className={"mx-auto h-5 w-5"}
+                    />
+                </div>
+            );
+        },
     },
 ];
