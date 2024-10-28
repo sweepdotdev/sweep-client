9169b777cba4a02e945e1243cbd6f328ba46c5e5
metal-face <hughesbryan3000@gmail.com>
2024-10-28 16:13:16 -0400

    Typing added

--- src/components/change-requests/table/data-table.tsx
+++ src/components/change-requests/table/data-table.tsx
@@ -6,6 +6,10 @@ import {
     VisibilityState,
     SortingState,
     getSortedRowModel,
+    Row,
+    HeaderGroup,
+    Header,
+    Cell,
 } from "@tanstack/react-table";
 
 import type { Table as TanstackTable } from "@tanstack/react-table";
@@ -106,40 +110,48 @@ export function DataTable<TData, TValue>({
             <div className={"rounded-md border shadow-2xl"}>
                 <Table>
                     <TableHeader>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => {
-                                    return (
-                                        <TableHead key={header.id}>
-                                            {header.isPlaceholder
-                                                ? null
-                                                : flexRender(
-                                                      header.column.columnDef.header,
-                                                      header.getContext(),
-                                                  )}
-                                        </TableHead>
-                                    );
-                                })}
-                            </TableRow>
-                        ))}
+                        {table.getHeaderGroups().map(
+                            (headerGroup: HeaderGroup<TData>): ReactElement => (
+                                <TableRow key={headerGroup.id}>
+                                    {headerGroup.headers.map(
+                                        (header: Header<TData, unknown>): ReactElement => {
+                                            return (
+                                                <TableHead key={header.id}>
+                                                    {header.isPlaceholder
+                                                        ? null
+                                                        : flexRender(
+                                                              header.column.columnDef.header,
+                                                              header.getContext(),
+                                                          )}
+                                                </TableHead>
+                                            );
+                                        },
+                                    )}
+                                </TableRow>
+                            ),
+                        )}
                     </TableHeader>
                     <TableBody>
                         {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => (
-                                <TableRow
-                                    key={row.id}
-                                    data-state={row.getIsSelected() && "selected"}
-                                >
-                                    {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext(),
-                                            )}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))
+                            table.getRowModel().rows.map(
+                                (row: Row<TData>): ReactElement => (
+                                    <TableRow
+                                        key={row.id}
+                                        data-state={row.getIsSelected() && "selected"}
+                                    >
+                                        {row.getVisibleCells().map(
+                                            (cell: Cell<TData, unknown>): ReactElement => (
+                                                <TableCell key={cell.id}>
+                                                    {flexRender(
+                                                        cell.column.columnDef.cell,
+                                                        cell.getContext(),
+                                                    )}
+                                                </TableCell>
+                                            ),
+                                        )}
+                                    </TableRow>
+                                ),
+                            )
                         ) : (
                             <TableRow>
                                 <TableCell colSpan={columns.length} className="h-24 text-center">
