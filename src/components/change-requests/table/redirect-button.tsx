d6ae551e4ef7fb3db6db05eeb8da37a6ff907cc2
metal-face <hughesbryan3000@gmail.com>
2024-10-28 21:01:18 -0400

    Changed to 'view' for now

--- src/components/change-requests/table/redirect-button.tsx
+++ src/components/change-requests/table/redirect-button.tsx
@@ -1,6 +1,5 @@
 import { ReactElement } from "react";
 import { Button } from "@/components/ui/button";
-import { SquareArrowUpRight } from "lucide-react";
 import { NavigateFunction, useNavigate } from "react-router-dom";
 
 interface Props {
@@ -10,12 +9,8 @@ export default function RedirectButton({ id }: Props): ReactElement {
     const redirect: NavigateFunction = useNavigate();
     return (
         <div className={"flex items-center"}>
-            <Button
-                size={"icon"}
-                variant={"link"}
-                onClick={() => redirect(`/change-requests/${id}`)}
-            >
-                <SquareArrowUpRight className={"h-5 w-5"} />
+            <Button variant={"link"} onClick={() => redirect(`/change-requests/${id}`)}>
+                View
             </Button>
         </div>
     );
