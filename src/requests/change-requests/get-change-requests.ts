09ce21fb69a94a4fe298292235ca0635fb0dd4fb
metal-face <hughesbryan3000@gmail.com>
2024-10-30 21:28:20 -0400

    pagination parameters added to request

--- src/requests/change-requests/get-change-requests.ts
+++ src/requests/change-requests/get-change-requests.ts
@@ -1,9 +1,14 @@
 import { Axios } from "@/lib/axios";
 
-export async function getAllChangeRequests() {
+interface GetChangeRequests {
+    page: number;
+    pageSize: number;
+}
+
+export async function getAllChangeRequests({ page, pageSize }: GetChangeRequests) {
     return Axios({
         method: "GET",
-        url: `http://localhost:8000/v1/change_requests`,
+        url: `${import.meta.env.VITE_BACKEND_BASE_URL}/v1/change_requests?page=${page}&pageSize=${pageSize}}`,
         withCredentials: true,
         headers: {
             "Content-Type": "application/json",
