89518c0ae23fb13d0eaf27869246edd6dedb2e6c
metal-face <hughesbryan3000@gmail.com>
2024-11-01 09:34:12 -0400

    mutation defined to make axios request to upload profile picture

---
+++ src/requests/user/use-upload-avatar.tsx
@@ -0,0 +1,28 @@
+import { useMutation } from "@tanstack/react-query";
+import { uploadAvatar, UploadPayload } from "@/requests/user/upload-profile-picture";
+import { useToast } from "@/hooks/use-toast";
+import { AxiosResponse } from "axios";
+
+export default function useUploadAvatar() {
+    const { toast } = useToast();
+
+    return useMutation({
+        mutationKey: ["uploadAvatar"],
+        mutationFn: async ({ file }: UploadPayload): Promise<AxiosResponse> =>
+            await uploadAvatar({ file }),
+        onSuccess: async (): Promise<void> => {
+            toast({
+                title: "Success 🎉",
+                description: "Your profile picture has been successfully uploaded!",
+                className: "bg-green-500",
+            });
+        },
+        onError: async (): Promise<void> => {
+            toast({
+                title: "Oops! 😬",
+                description: "Something went wrong!",
+                variant: "destructive",
+            });
+        },
+    });
+}
