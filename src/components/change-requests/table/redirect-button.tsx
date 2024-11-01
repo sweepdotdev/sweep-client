34d147fe0fe8bc15d726775cab158319434fdce2
metal-face <hughesbryan3000@gmail.com>
2024-10-31 21:12:25 -0400

    icon added to button

--- src/components/change-requests/table/redirect-button.tsx
+++ src/components/change-requests/table/redirect-button.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
 import { Button } from "@/components/ui/button";
 import { NavigateFunction, useNavigate } from "react-router-dom";
+import { SquareArrowOutUpRight } from "lucide-react";
 
 interface Props {
     id: string;
@@ -11,6 +12,7 @@ export default function RedirectButton({ id }: Props): ReactElement {
         <div className={"flex items-center"}>
             <Button variant={"link"} onClick={() => redirect(`/change-requests/${id}`)}>
                 View
+                <SquareArrowOutUpRight className={"ml-2 h-3 w-3"} />
             </Button>
         </div>
     );
