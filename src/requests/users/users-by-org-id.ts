2577569a8b53b7286769e770890b394082e4b1a2
metal-face <hughesbryan3000@gmail.com>
2024-11-06 14:57:21 -0500

    request updated to have org ID

--- src/requests/users/users-by-org-id.ts
+++ src/requests/users/users-by-org-id.ts
@@ -1,10 +1,19 @@
 import { Axios } from "@/lib/axios";
 import { AxiosResponse } from "axios";
 
-export async function getUsersByOrganizationId(): Promise<AxiosResponse> {
+interface Parameters {
+    organizationId: string;
+}
+
+export async function getUsersByOrganizationId({
+    organizationId,
+}: Parameters): Promise<AxiosResponse> {
     return Axios({
         method: "GET",
-        url: `${import.meta.env.VITE_BACKEND_BASE_URL}/v1/organizations/users`,
+        url: `${import.meta.env.VITE_BACKEND_BASE_URL}/v1/organizations/${organizationId}/users`,
+        headers: {
+            "Content-Type": "application/json",
+        },
         withCredentials: true,
     });
 }
