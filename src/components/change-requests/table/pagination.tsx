dee1850041db708fa7db9aa97f420042133a5f3d
metal-face <hughesbryan3000@gmail.com>
2024-11-14 00:03:51 -0500

    pagination spacing fixed

--- src/components/change-requests/table/pagination.tsx
+++ src/components/change-requests/table/pagination.tsx
@@ -36,7 +36,7 @@ export function DataTablePagination<TData>({ table, pageCount }: DataTablePagina
     }
     return (
         <div className="flex items-center justify-between px-2">
-            <div className="flex items-center space-x-6 lg:space-x-8">
+            <div className="w-full flex justify-between items-center">
                 <div className="flex items-center space-x-2">
                     <p className="text-sm font-medium">Rows per page</p>
                     <Select
