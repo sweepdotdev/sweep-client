b4d91eeeb2aca58fc4d5fe8caaeb7ac7af957124
metal-face <hughesbryan3000@gmail.com>
2024-11-21 00:22:35 -0500

    import renamed

--- src/requests/user/use-upload-avatar.tsx
+++ src/requests/user/use-upload-avatar.tsx
@@ -1,5 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-import { uploadAvatar, UploadPayload } from "@/requests/user/upload-profile-picture";
+import { uploadAvatar, UploadPayload } from "@/requests/user/upload-avatar";
 import { useToast } from "@/hooks/use-toast";
 import { AxiosResponse } from "axios";
 
