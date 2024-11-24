d1b15ace3a53156b5f3b9a55d32c1b6205c8bf7c
metal-face <hughesbryan3000@gmail.com>
2024-11-23 20:01:30 -0500

    hasOAuth2Credentials method given param

--- src/models/zustand/session-actions.ts
+++ src/models/zustand/session-actions.ts
@@ -5,7 +5,7 @@ export interface SessionActions {
     clearState: () => void;
     getState: () => SessionState;
     setState: (val: SessionState) => void;
-    setHasOAuth2Credentials: () => void;
+    setHasOAuth2Credentials: (has: boolean) => void;
     setClientSecret: (clientSecret: string) => void;
     setAvatarUrl: (avatarUrl: string) => void;
 }
